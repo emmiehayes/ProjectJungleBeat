bb46db790271cd7a419ab9d951f5e3357cd16bf0
Emmie Hayes <emmiehayes2@gmail.com>
2018-03-22 08:01:21 -0600

    Format updated

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,11 +12,11 @@ attr_accessor :head
       @head = Node.new(data)
     else
       current = @head
-        until current.next_node == nil
+      until current.next_node == nil
         current = current.next_node
         current.next_node = Node.new(data)
       end
-      end
+    end
     data
   end
 
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -10,38 +10,45 @@ class LinkedListTest < MiniTest::Test
 
   def test_it_exists
     list = LinkedList.new
+
     assert_instance_of LinkedList, list
   end
 
   def test_instance_nil
      list = LinkedList.new
+
      assert_equal nil, list.head
   end
 
   def test_instance_data
      list = LinkedList.new("doop")
+
      assert_equal "doop", list.head
   end
 
   def test_append_data
     list = LinkedList.new
+
     assert_equal "doop", list.append("doop")
   end
 
   def test_append_more_data
     list = LinkedList.new
     list.append("doop")
+
     assert_equal "deep", list.append("deep")
   end
 
   def test_count_head_is_nil
     list = LinkedList.new
+
     assert_equal 0, list.count
   end
 
   def test_count_when_count_one
     list = LinkedList.new
     list.append("doop")
+
     assert_equal 1, list.count("doop")
   end
 
@@ -49,12 +56,14 @@ class LinkedListTest < MiniTest::Test
     list = LinkedList.new
     list.append("doop")
     list.append("deep")
+
     assert_equal 2, list.count
   end
 
   def test_to_string_one_word
     skip
     list.append("doop")
+    
     assert_equal "doop", list.to_string
   end
 
@@ -62,6 +71,7 @@ class LinkedListTest < MiniTest::Test
     skip
     list.append("doop")
     list.append("deep")
+
     assert_equal "doop, deep", list.to_string
   end
 
